4ae70709ac2a50ebdf69fdc3b76d9f64b330c9cd
wangzl1163 <wangzl1163@qq.com>
2024-01-26 16:37:56 +0800

    style：规范命名

--- license.js
+++ license.js
@@ -1,19 +1,20 @@
-const package = require('./package.json')
-const date = new Date()
-let dd = date.getDate()
-let mm = date.getMonth() + 1 // January is 0!
-const yyyy = date.getFullYear()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+const date = new Date();
+let dd = date.getDate();
+let mm = date.getMonth() + 1; // January is 0!
+const yyyy = date.getFullYear();
 
 if (dd < 10) {
-   dd = '0' + dd
+   dd = '0' + dd;
 }
 
 if (mm < 10) {
-   mm = '0' + mm
+   mm = '0' + mm;
 }
 
-const today = dd + '/' + mm + '/' + yyyy
+const today = dd + '/' + mm + '/' + yyyy;
 
-exports.banner = `@license :${package.name} - V${package.version} - ${today}
-https://github.com/wangzl1163/${package.name}
-Copyright (c) ${yyyy} @wangzl1163; Licensed ${package.license}`
+exports.banner = `@license :${packageJson.name} - V${packageJson.version} - ${today}
+https://github.com/wangzl1163/${packageJson.name}
+Copyright (c) ${yyyy} @wangzl1163; Licensed ${packageJson.license}`;
