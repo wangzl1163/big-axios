1e7b0b969289a39ebf1910e96c186cf0b1dbf626
wangzl1163 <wangzl1163@qq.com>
2024-01-26 16:33:13 +0800

    fix: 修复构建配置不正确

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -3,20 +3,14 @@ const webpack = require('webpack');
 const baseConfig = require('./webpack.base.config');
 
 module.exports = merge(baseConfig, {
-   devtool: 'source-map',
    mode: 'production',
    output: {
       filename: 'big-axios.js',
       chunkFilename: '[name].chunk.js',
       sourceMapFilename: 'big-axios.map'
    },
-   plugins: [
-      new webpack.DefinePlugin({
-         'process.env': {
-            NODE_ENV: JSON.stringify('development')
-         }
-      })
-   ],
+   devtool: 'source-map',
+   plugins: [],
    optimization: {
       minimize: false
    }
